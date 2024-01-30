9734f5671b8454dd97da04530aceb0585f45cbf6
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-01-30 20:58:47 +0700

    add Text

--- Animation.java
+++ Animation.java
@@ -1,13 +1,31 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Animation extends JPanel implements Runnable{
+public class Animation extends JPanel implements Runnable,MouseListener{
+
+    static Font font;
+    Animation(){
+        addMouseListener(this);
+    }
     public static void main(String[] args) {
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("pokemon-emerald.ttf")).deriveFont(50f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("pokemon-emerald.ttf")));
+        } catch (Exception e) {
+            System.err.println("NO FONT");
+        }
+
         Animation m = new Animation();
         JFrame f = new JFrame();
         f.add(m);
@@ -34,50 +52,26 @@ public class Animation extends JPanel implements Runnable{
         double squareVelocity = -100;
         
         startTime = lastTime;
+
         while (true)
         {
             currentTime = System.currentTimeMillis();
             elapsedTime = currentTime - lastTime;
             lastTime = currentTime;
             
-            updateText();
+            double timer = (currentTime-startTime)/1000.0;   //timer since start running the animation in second unit
+            if(timer <= 3){
 
-            //move circle
-            circleMove += circleVelocity * elapsedTime / 1000.0;
-            //rotate square
-            squareRotate += 0.5 * elapsedTime / 1000.0;
-            //move square
-            if((currentTime-startTime)/1000.0 >= 3)
-                squareMove += squareVelocity * elapsedTime / 1000.0;
+            }
+            else if(timer <= 6){
 
-            //check cirlce
-            if(circleMove >= 500){
-                circleMove = 500;
-                circleVelocity = -circleVelocity;
             }
-            else if(circleMove <= 0){
-                circleMove = 0;
-                circleVelocity = -circleVelocity;
-            }
+        
 
-            //check square
-            if(squareMove >= 500){
-                squareMove = 500;
-                squareVelocity = -squareVelocity;
-            }
-            else if(squareMove <= 0){
-                squareMove = 0;
-                squareVelocity = -squareVelocity;
-            }
 
             //Display
             repaint();
         }
-    }
-
-    private void updateText() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateText'");
     }
 
     @Override
@@ -85,12 +79,37 @@ public class Animation extends JPanel implements Runnable{
         Graphics2D g2 = (Graphics2D) g;
         drawBackground(g2);
         drawTextbox(g2);
+        drawText(g2);
         drawEffect(g2);
         drawBaby(g2);
+        // g2.setColor(Color.BLACK);
+        // g2.translate(circleMove, 0);
+        // g2.drawOval(0, 0, 100, 100);
+        // //rotate square
+        // g2.translate(-circleMove, 0);
+        // g2.rotate(squareRotate, 300, 300);
+        // g2.drawRect(200, 200, 200, 200);
+
+        // //move square
+        // g2.rotate(-squareRotate, 300, 300);
+        // g2.translate(0, squareMove);
+        // g2.drawRect(0, 0, 100, 100);
     }
    
-    private void drawBackground(Graphics g) {
+    private void drawText(Graphics2D g) {
+        g.setFont(font);
 
+        g.setColor(new Color(86, 73, 84));
+        g.drawString("What?", 38, 514);
+        g.drawString("Baby Chicken is evolving!", 38, 564);
+
+        g.setColor(new Color(200, 194, 205));
+        g.drawString("What?", 35, 510);
+        g.drawString("Baby Chicken is evolving!", 35, 560);
+    }
+
+    private void drawBackground(Graphics2D g) {
+        //clean screen
         g.setColor(new Color(0,0,0));
         g.fillRect(0, 0, 600, 600);
 
@@ -120,7 +139,7 @@ public class Animation extends JPanel implements Runnable{
 
     }
     
-    private void drawTextbox(Graphics g) {
+    private void drawTextbox(Graphics2D g) {
 
         g.setColor(new Color(62,57,70));
         g.fillRect(0, 450, 600, 150);
@@ -128,17 +147,59 @@ public class Animation extends JPanel implements Runnable{
         g.setColor(new Color(150,58,54));
         g.fillRoundRect(10, 455, 580, 140, 10, 10);
 
-        g.setColor(new Color(104,138,134));
+        g.setColor(new Color(93, 138, 147));
         g.fillRoundRect(20, 460, 560, 130, 10, 10);
 
     }
     
-    private void drawEffect(Graphics g) {
+    private void drawEffect(Graphics2D g) {
         
     }
 
-    private void drawBaby(Graphics g) {
+    private void drawBaby(Graphics2D g) {
         
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Click at "+ e.getX() +", "+e.getY());
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
     
 }
